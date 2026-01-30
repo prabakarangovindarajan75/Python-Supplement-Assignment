fd7a4eb8af4b731592d47a7b2df2465372dec7ed
prabakarangovindarajan75 <prabakaran.g_25cs@kgkite.ac.in>
2026-01-30 11:49:12 +0530

    Update problem_02.py

--- problem_02.py
+++ problem_02.py
@@ -1,7 +1,4 @@
-# Problem 2: Calculate the sum of numbers from 1 to 100
-# Find and fix the error
-
 total = 0
-for i in range(1, 100):
+for i in range(1, 101):
     total += i
 print(f"Sum is: {total}")
