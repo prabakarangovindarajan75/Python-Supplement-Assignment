f6bce6ecade3e883e6d1206c84e960aa1f0a66c4
prabakarangovindarajan75 <prabakaran.g_25cs@kgkite.ac.in>
2026-01-30 20:02:56 +0530

    Update problem_58.py

--- problem_58.py
+++ problem_58.py
@@ -3,14 +3,17 @@
 
 def binary_search(arr, target):
     left, right = 0, len(arr) - 1
+
     while left <= right:
         mid = (left + right) // 2
+
         if arr[mid] == target:
             return mid
         elif arr[mid] < target:
-            left = mid
+            left = mid + 1      # <-- fix here
         else:
-            right = mid
+            right = mid - 1     # <-- fix here
+
     return -1
 
 numbers = [1, 3, 5, 7, 9, 11, 13, 15]
