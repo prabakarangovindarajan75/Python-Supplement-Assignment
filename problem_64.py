f59329299bdf1d852a031c030f4f6769f04630b3
prabakarangovindarajan75 <prabakaran.g_25cs@kgkite.ac.in>
2026-01-30 20:09:04 +0530

    Update problem_64.py

--- problem_64.py
+++ problem_64.py
@@ -5,7 +5,7 @@ def merge_sorted(list1, list2):
     merged = []
     i, j = 0, 0
     while i < len(list1) and j < len(list2):
-        if list1[i] < list2[j]:
+        if list1[i] <= list2[j]:  # Fix: use <=
             merged.append(list1[i])
             i += 1
         else:
@@ -18,3 +18,4 @@ def merge_sorted(list1, list2):
 l1 = [1, 3, 5]
 l2 = [2, 4, 6]
 print(f"Merged: {merge_sorted(l1, l2)}")
+
