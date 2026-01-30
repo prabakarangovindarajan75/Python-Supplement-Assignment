7b5783a460c205262c39e99900ad8acfaf4becaf
prabakarangovindarajan75 <prabakaran.g_25cs@kgkite.ac.in>
2026-01-30 20:26:40 +0530

    Update problem_91.py

--- problem_91.py
+++ problem_91.py
@@ -3,13 +3,10 @@
 
 def selection_sort(arr):
     n = len(arr)
-    for i in range(n):
+    for i in range(n-1):  # last element will already be in place
         min_idx = i
         for j in range(i+1, n):
             if arr[j] < arr[min_idx]:
                 min_idx = j
         arr[i], arr[min_idx] = arr[min_idx], arr[i]
     return arr
-
-numbers = [64, 25, 12, 22, 11]
-print(f"Sorted: {selection_sort(numbers)}")
